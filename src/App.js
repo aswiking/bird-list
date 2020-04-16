4288d5ab42dec0bba3fd695acbe89f6d8aeea64c
Sofia Wiking <aswiking@gmail.com>
2020-04-16 15:02:21 +0100

    add fetch request

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BirdForm from "./bird-form";
 import cuid from "cuid";
 
 function App() {
-  const [birdData, setBirds] = useState([
-    {
-      id: 1,
-      name: "blackbird",
-      scientific: "Turdus merula",
-      location: "Leicester",
-      date: "13 April 2020",
-      image: "https://twootz.com/assets/images/bird/Blackbird.jpg",
-    },
-    {
-      id: 2,
-      name: "robin",
-      scientific: "Erithacus rubecula",
-      location: "Leicester",
-      date: "10 January 2020",
-      image:
-        "https://images.immediate.co.uk/production/volatile/sites/23/2014/12/GettyImages-511380252-08b8a2e.jpg",
-    },
-  ]);
+  const [birdData, setBirds] = useState([]);
+
+  useEffect(() => {
+    async function fetchBirds() {
+      const res = await fetch("/api/birds");
+      const birds = await res.json();
+      setBirds(birds)
+    };
+    fetchBirds();
+
+  }, []);
 
   const birdList = birdData.map((bird) => {
     if (!bird.isEditing) {
@@ -45,7 +37,9 @@ function App() {
             <button onClick={(event) => setEditing(event, bird.id)}>
               Edit
             </button>
-            <button onClick={(event) => deleteBird(event, bird.id)}>Delete</button>
+            <button onClick={(event) => deleteBird(event, bird.id)}>
+              Delete
+            </button>
           </div>
         </div>
       );
@@ -74,7 +68,7 @@ function App() {
     event.preventDefault();
     console.log(newBird);
     setBirds(
-      birdData.map((bird => {
+      birdData.map((bird) => {
         if (bird.id === newBird.id) {
           return {
             id: newBird.id,
@@ -83,10 +77,12 @@ function App() {
             location: event.target.location.value,
             date: event.target.date.value,
             image: event.target.url.value,
-          }
-        } else {return bird}
-      }))
-    )
+          };
+        } else {
+          return bird;
+        }
+      })
+    );
   }
 
   function setEditing(event, id) {
@@ -102,9 +98,7 @@ function App() {
   }
 
   function deleteBird(event, id) {
-    setBirds(
-      birdData.filter(bird => bird.id !== id)
-    )
+    setBirds(birdData.filter((bird) => bird.id !== id));
   }
 
   return (
