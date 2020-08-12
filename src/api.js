4b7671372a17d64392a81b2a00b74f22d2783ecc
Sofia Wiking <aswiking@gmail.com>
2020-08-12 11:36:07 +0100

    added function for generic error handling

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export default async function apiFetch(url, options, errorMessage) {
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    const error = await res.json();
+    error.message = errorMessage;
+    throw error;
+  }
+  return res;
+}
