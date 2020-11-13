627e1b029e2d3b795861f201c76e1c0b34054808
Sofia Wiking <aswiking@gmail.com>
2020-11-13 15:30:35 +0000

    some basic styling of family component

--- src/Family.js
+++ src/Family.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import './Family.scss';
 
 export default function Family(props) {
 
   const familyName = props.categoryData[0];
 
   const speciesList = props.categoryData[1].map((bird, index) => {
-    return <div key={index}>
+    return <div key={index} className="bird-entry">
       {(bird.sighting_ids.length !== 0 ) && <FontAwesomeIcon icon={faCheck} className="tickIcon" alt='You have seen this bird' size="2x"/>}
       <div className="bird-details"><h3>{bird.common}</h3>
       <h4 className="scientific">{bird.scientific}</h4>
@@ -16,7 +17,7 @@ export default function Family(props) {
   })
 
 return <div>
-  <h2>{familyName}</h2>
+  <h2 className="family-name">{familyName}</h2>
   {speciesList}
 
 </div>
