bf102bba085e4285248c4dff412e1031de047607
Sofia Wiking <aswiking@gmail.com>
2020-12-01 16:35:31 +0000

    added filter

--- src/AllBirds.js
+++ src/AllBirds.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import apiFetch from "./api";
-import Family from './Family.js';
+import Family from "./Family.js";
+import Header from "./Header.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./AllBirds.scss";
 
 export default function AllBirds(props) {
   const { currentUser, setError } = props;
 
   const [birdData, setBirdData] = useState([]);
+  const [displayingBirds, setDisplayingBirds] = useState({
+    seen: true,
+    unseen: true,
+  });
+  const [displayMenu, setDisplayMenu] = useState(false);
 
   useEffect(() => {
     async function fetchBirds() {
@@ -49,28 +57,119 @@ export default function AllBirds(props) {
     fetchBirds();
   }, []);
 
+  function toggleDisplayMenu() {
+    setDisplayMenu(!displayMenu);
+  }
+
+  function toggleDisplayingBirds(event) {
+    console.log(event.target.id, event.target.checked);
+    if (event.target.id === "seen") {
+      if (event.target.checked) {
+        setDisplayingBirds({
+          ...displayingBirds,
+          seen: true,
+        });
+      } else {
+        setDisplayingBirds({
+          ...displayingBirds,
+          seen: false,
+        });
+      }
+    } else if (event.target.id === "unseen") {
+      if (event.target.checked) {
+        setDisplayingBirds({
+          ...displayingBirds,
+          unseen: true,
+        });
+      } else {
+        setDisplayingBirds({
+          ...displayingBirds,
+          unseen: false,
+        });
+      }
+    }
+  }
+
+  let filteredBirdData = birdData.filter(
+    (bird) =>
+      (displayingBirds.seen && (bird.sighting_ids.length !== 0)) ||
+      (displayingBirds.unseen && (bird.sighting_ids.length === 0))
+  );
+
   let categories = {};
 
-  birdData.forEach((bird) => {
+  filteredBirdData.forEach((bird) => {
     if (!(bird.group.name in categories)) {
-      categories[bird.group.name] = [bird]
+      categories[bird.group.name] = [bird];
     } else {
-      categories[bird.group.name].push(bird)
+      categories[bird.group.name].push(bird);
     }
   });
 
+  const birdList = Object.entries(categories).map((family, index) => {
+    return <Family categoryData={family} key={index} className="category" />;
+  });
 
- const birdList = Object.entries(categories).map((family, index) => {
-   return <Family categoryData={family} key={index}/>
- });
+  const categoryList = Object.entries(categories).map((family, index) => {
+    return (
+      <a key={index} href={`#${family[0]}`} onClick={() => toggleDisplayMenu()}>
+        <li >{family[0]}</li>
+      </a>
+    );
+  });
 
- const categoryList = Object.entries(categories).map((family, index) => {
-  return <a href={`#${family[0]}`}><li key={index} >{family[0]}</li></a>
-});
-
-
-return <div className="all-birds">
-  <ul className="category-list" >{categoryList}</ul>
-<ul className="bird-table">{birdList}</ul>
-</div>;
+  return (
+    <div className="all-birds">
+      <Header />
+      <div className="category-menu">
+        <div className="scroller">
+          <h3
+            className="category-menu-header"
+            onClick={() => toggleDisplayMenu()}
+          >
+            Categories and filters
+            {!displayMenu ? (
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="down-arrow"
+                alt="list of birds"
+                size="2x"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faTimes}
+                className="cross"
+                alt="list of birds"
+                size="2x"
+              />
+            )}
+          </h3>
+          {displayMenu && (
+            <div>
+              <div className="filters">
+                <form>
+                  <label htmlFor="seen">Birds I've seen</label>
+                  <input
+                    type="checkbox"
+                    id="seen"
+                    onChange={(event) => toggleDisplayingBirds(event)}
+                    checked={displayingBirds.seen}
+                  ></input>
+                  <label htmlFor="unseen">Birds I've not seen</label>
+                  <input
+                    type="checkbox"
+                    id="unseen"
+                    onChange={(event) => toggleDisplayingBirds(event)}
+                    checked={displayingBirds.unseen}
+                  ></input>
+                </form>
+              </div>
+              <ul className="category-list">{categoryList}</ul>
+            </div>
+          )}
+        </div>
+      </div>
+      <ul className="bird-table">{birdList}</ul>
+    </div>
+  );
 }
