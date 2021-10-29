2928e5488f2c030753c764d76c08e279dd5b3552
Sofia Wiking <aswiking@gmail.com>
2021-10-29 15:11:00 +0100

    Change to hard coded url

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -115,7 +115,7 @@ export default function LoginPage(props) {
         <div className="login-options">
         <p className="login-options-title">Log in or sign up</p>
           <a
-            href={`https://api.instagram.com/oauth/authorize?client_id=1440877326102459&redirect_uri=${(process.env.NODE_ENV === 'production') ? process.env.URL : 'https://localhost:3000'}/&scope=user_profile,user_media&response_type=code`}
+            href={`https://api.instagram.com/oauth/authorize?client_id=1440877326102459&redirect_uri=${(process.env.NODE_ENV === 'production') ? 'https://aswiking-fledgling.netlify.app' : 'https://localhost:3000'}/&scope=user_profile,user_media&response_type=code`}
           >
             <div className="button-container">
               <button className="instagram-button hover-pointer">
